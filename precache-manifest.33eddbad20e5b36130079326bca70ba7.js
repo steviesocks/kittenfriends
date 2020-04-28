6a0bf9e3be07c185fc81f7f281122a0947002bf7
steviesocks <stephen.stocking@gmail.com>
2020-04-28 18:03:57 -0400

    Updates

---
+++ precache-manifest.33eddbad20e5b36130079326bca70ba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2941ebbd08980443a74f997a7387fe19",
+    "url": "/kittenfriends/index.html"
+  },
+  {
+    "revision": "343b1928e2a53aebe456",
+    "url": "/kittenfriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "298e0e2f62cc8644a2a3",
+    "url": "/kittenfriends/static/css/main.3c774703.chunk.css"
+  },
+  {
+    "revision": "343b1928e2a53aebe456",
+    "url": "/kittenfriends/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittenfriends/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298e0e2f62cc8644a2a3",
+    "url": "/kittenfriends/static/js/main.99ce5079.chunk.js"
+  },
+  {
+    "revision": "4946e23ef0194e55103f",
+    "url": "/kittenfriends/static/js/runtime-main.a2178f0f.js"
+  },
+  {
+    "revision": "372cc1d42335e38f4cf56df6091a73cb",
+    "url": "/kittenfriends/static/media/CHERI___.372cc1d4.woff"
+  }
+]);
